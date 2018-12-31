c71a25a49e24bfb194c49b1582f1fbda037976ed
zhangtao <7253641@qq.com>
2018-12-31 17:35:49 +0800

    config.js modified

--- config.js
+++ config.js
@@ -56,7 +56,7 @@ var config = {
                 },
                 {
                   type: 'switch.widgets',
-                  columns: 2, rows: 2,
+                  columns: 2, rows: 3,
                   x: 1, y: 0,
                   duration: 8000,
                   widgets: [
@@ -81,56 +81,56 @@ var config = {
                       title: '中国·舟山水产品指数',
                       url: 'http://www.scpzs.com/',
                     },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·永康五金指数',
-                    //   url: 'http://www.ykindex.com',
-                    // },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·四季青服装指数',
-                    //   url: 'zs.zjpi.gov.cn/market/index/shijiqin',
-                    // },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·金恒德汽车用品价格指数',
-                    //   url: 'http://www.jhdindex.com',
-                    // },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '浙江省菜篮子指数',
-                    //   url: 'http://zs.zjpi.gov.cn/market/index/cailanzi',
-                    // },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·中关村电子信息产品指数',
-                    //   url: 'http://www.zgcindex.org',
-                    // },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·临沂商城价格指数',
-                    //   url: 'http://www.lyscprice.com',
-                    // },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·金乡大蒜指数',
-                    //   url: 'http://www.jxdsi.com',
-                    // },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·寿光蔬菜指数',
-                    //   url: 'http://www.sgvindex.com',
-                    // },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·昌邑生姜指数',
-                    //   url: '',
-                    // },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·成都中药材指数',
-                    //   url: 'http://www.ysindex.com',
-                    // }
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·永康五金指数',
+                      url: 'http://www.ykindex.com',
+                    },
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·四季青服装指数',
+                      url: 'zs.zjpi.gov.cn/market/index/shijiqin',
+                    },
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·金恒德汽车用品价格指数',
+                      url: 'http://www.jhdindex.com',
+                    },
+                    {
+                      type: 'iframe.iframe',
+                      title: '浙江省菜篮子指数',
+                      url: 'http://zs.zjpi.gov.cn/market/index/cailanzi',
+                    },
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·中关村电子信息产品指数',
+                      url: 'http://www.zgcindex.org',
+                    },
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·临沂商城价格指数',
+                      url: 'http://www.lyscprice.com',
+                    },
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·金乡大蒜指数',
+                      url: 'http://www.jxdsi.com',
+                    },
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·寿光蔬菜指数',
+                      url: 'http://www.sgvindex.com',
+                    },
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·昌邑生姜指数',
+                      url: '',
+                    },
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·成都中药材指数',
+                      url: 'http://www.ysindex.com',
+                    }
                   ]
               },
               // {
