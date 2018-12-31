ffbbba93a78207765e174404eb24a56fd922427e
zhangtao <7253641@qq.com>
2018-12-31 17:16:32 +0800

    config.js modified

--- config.js
+++ config.js
@@ -49,7 +49,8 @@ var config = {
                 {
                     type: 'iframe.iframe',
                     title: '全国指数项目分布图',
-                    url: 'https://indexmap.herokuapp.com',
+                    // url: 'https://indexmap.herokuapp.com',
+                    url: 'http://127.0.0.1:3000'
                     columns: 1, rows: 2,
                     x: 0, y: 0
                 },
