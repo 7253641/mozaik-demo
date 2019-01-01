f3386f59aaba6d203b36401705a6131626ecb42e
zhangtao <7253641@qq.com>
2019-01-01 18:43:07 +0800

    添加chart

---
+++ src/chart/ui/components/BarChartWidget.jsx
@@ -0,0 +1,60 @@
+import React, {Component, PropTypes} from 'react';
+import Mozaik                          from 'mozaik/browser';
+const {BarChart}                     = Mozaik.Component;
+
+
+class BarChartWidget extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let builds = this.props.data;
+        var title = this.props.title;
+        let barChartOptions = {
+            mode: 'stacked',
+            xLegend: '城 市',
+            xLegendPosition: 'right',
+            yLegend: '数 量',
+            yLegendPosition: 'top',
+            xPadding: 0.1,
+            transitionDuration: 60,
+            barColor: "#aadcff",
+            barClass: function (d) {
+                return `result--${ builds }`;
+            }
+        };
+
+        return (
+            <div>
+                <div className="widget__header">
+                    {title}
+                    <i className="fa fa-bar-chart"/>
+                </div>
+                <div className="widget__body">
+                    <BarChart data={[{data: builds}]} options={barChartOptions}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+// BarChartWidget.propTypes = {
+//     title: React.PropTypes.string,
+//     dateFormat: React.PropTypes.string,
+//     min: React.PropTypes.integer,
+//     max: React.PropTypes.integer,
+//     tickCount: React.PropTypes.integer,
+//     data: React.PropTypes.string.isRequired
+// };
+// BarChartWidget.defaultProps = {
+//     title: 'Bar Chart widget',
+//     dateFormat: 'YYYY-MM-DD',
+//     min: null,
+//     max: null,
+//     tickCount: null
+// };
+
+export default  BarChartWidget;
---
+++ src/chart/ui/index.js
@@ -0,0 +1,8 @@
+// import ChartWidget from './ChartWidget.jsx';
+import LineChartWidget from './components/LineChartWidget.jsx';
+import BarChartWidget from './components/BarChartWidget.jsx';
+
+export default {
+  BarChartWidget,
+  LineChartWidget,
+}
