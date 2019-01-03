583e2163a98038fb4296d8a28098cb8ad75b44fb
zhangtao <7253641@qq.com>
2019-01-03 17:12:52 +0800

    delete png

--- config.js
+++ config.js
@@ -68,83 +68,83 @@ var config = {
                   x: 1, y: 0,
                   duration: 10000,
                   widgets: [
-                    {
-                      type: 'image.image',
-                      title: '中国·义乌小商品指数',
-                      url: 'https://github.com/7253641/mozaik-demo/raw/demo/images/www_ywindex_com.png',
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center -7vh',
-                    },
-                    {
-                      type: 'image.image',
-                      title: '中国·柯桥纺织指数',
-                      url: 'https://github.com/7253641/mozaik-demo/raw/demo/images/www_kqindex_cn.png',
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center -7vh',
-                    },
-                    {
-                      type: 'image.image',
-                      title: '中国·海宁皮革指数',
-                      url: 'https://github.com/7253641/mozaik-demo/raw/demo/images/www_hnindex_com.png',
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center -7vh',
-                    },
-                    {
-                      type: 'image.image',
-                      title: '中国·舟山水产品指数',
-                      url: 'https://github.com/7253641/mozaik-demo/raw/demo/images/www_scpzs_com.png',
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center -7vh',
-                    },
-                    {
-                      type: 'image.image',
-                      title: '中国·永康五金指数',
-                      url: 'https://github.com/7253641/mozaik-demo/raw/demo/images/www_ykindex_com.png',
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center -7vh',
-                    },
-                    {
-                      type: 'image.image',
-                      title: '中国·四季青服装指数',
-                      url: 'https://github.com/7253641/mozaik-demo/raw/demo/images/zs_zjpi_gov_cn_market_index_shijiqin.png',
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center -7vh',
-                    },
-                    {
-                      type: 'image.image',
-                      title: '中国·四季青服装指数',
-                      url: 'https://github.com/7253641/mozaik-demo/raw/demo/images/zs_zjpi_gov_cn_market_index_shijiqin.png',
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center -7vh',
-                    },
-                    {
-                      type: 'image.image',
-                      title: '浙江省菜篮子指数',
-                      url: 'https://github.com/7253641/mozaik-demo/raw/demo/images/zs_zjpi_gov_cn_market_index_cailanzi.png',
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center -7vh',
-                    },
-                    {
-                      type: 'image.image',
-                      title: '中国·金恒德汽车用品价格指数',
-                      url: 'https://github.com/7253641/mozaik-demo/raw/demo/www_jhdindex_com.png',
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center -7vh',
-                    },
-                    {
-                      type: 'image.image',
-                      title: '中国·中关村电子信息产品指数',
-                      url: 'https://github.com/7253641/mozaik-demo/raw/demo/www_zgcindex_org.png',
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center -7vh',
-                    },
-                    {
-                      type: 'image.image',
-                      title: '中国·临沂商城价格指数',
-                      url: 'https://github.com/7253641/mozaik-demo/raw/demo/www_lyscprice_com.png',
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center -7vh',
-                    },
+                    // {
+                    //   type: 'image.image',
+                    //   title: '中国·义乌小商品指数',
+                    //   url: 'https://github.com/7253641/mozaik-demo/raw/demo/images/www_ywindex_com.png',
+                    //   backgroundSize: 'cover',
+                    //   backgroundPosition: 'center -7vh',
+                    // },
+                    // {
+                    //   type: 'image.image',
+                    //   title: '中国·柯桥纺织指数',
+                    //   url: 'https://github.com/7253641/mozaik-demo/raw/demo/images/www_kqindex_cn.png',
+                    //   backgroundSize: 'cover',
+                    //   backgroundPosition: 'center -7vh',
+                    // },
+                    // {
+                    //   type: 'image.image',
+                    //   title: '中国·海宁皮革指数',
+                    //   url: 'https://github.com/7253641/mozaik-demo/raw/demo/images/www_hnindex_com.jpg',
+                    //   backgroundSize: 'cover',
+                    //   backgroundPosition: 'center -7vh',
+                    // },
+                    // {
+                    //   type: 'image.image',
+                    //   title: '中国·舟山水产品指数',
+                    //   url: 'https://github.com/7253641/mozaik-demo/raw/demo/images/www_scpzs_com.png',
+                    //   backgroundSize: 'cover',
+                    //   backgroundPosition: 'center -7vh',
+                    // },
+                    // {
+                    //   type: 'image.image',
+                    //   title: '中国·永康五金指数',
+                    //   url: 'https://github.com/7253641/mozaik-demo/raw/demo/images/www_ykindex_com.png',
+                    //   backgroundSize: 'cover',
+                    //   backgroundPosition: 'center -7vh',
+                    // },
+                    // {
+                    //   type: 'image.image',
+                    //   title: '中国·四季青服装指数',
+                    //   url: 'https://github.com/7253641/mozaik-demo/raw/demo/images/zs_zjpi_gov_cn_market_index_shijiqin.png',
+                    //   backgroundSize: 'cover',
+                    //   backgroundPosition: 'center -7vh',
+                    // },
+                    // {
+                    //   type: 'image.image',
+                    //   title: '中国·四季青服装指数',
+                    //   url: 'https://github.com/7253641/mozaik-demo/raw/demo/images/zs_zjpi_gov_cn_market_index_shijiqin.png',
+                    //   backgroundSize: 'cover',
+                    //   backgroundPosition: 'center -7vh',
+                    // },
+                    // {
+                    //   type: 'image.image',
+                    //   title: '浙江省菜篮子指数',
+                    //   url: 'https://github.com/7253641/mozaik-demo/raw/demo/images/zs_zjpi_gov_cn_market_index_cailanzi.png',
+                    //   backgroundSize: 'cover',
+                    //   backgroundPosition: 'center -7vh',
+                    // },
+                    // {
+                    //   type: 'image.image',
+                    //   title: '中国·金恒德汽车用品价格指数',
+                    //   url: 'https://github.com/7253641/mozaik-demo/raw/demo/www_jhdindex_com.png',
+                    //   backgroundSize: 'cover',
+                    //   backgroundPosition: 'center -7vh',
+                    // },
+                    // {
+                    //   type: 'image.image',
+                    //   title: '中国·中关村电子信息产品指数',
+                    //   url: 'https://github.com/7253641/mozaik-demo/raw/demo/www_zgcindex_org.png',
+                    //   backgroundSize: 'cover',
+                    //   backgroundPosition: 'center -7vh',
+                    // },
+                    // {
+                    //   type: 'image.image',
+                    //   title: '中国·临沂商城价格指数',
+                    //   url: 'https://github.com/7253641/mozaik-demo/raw/demo/www_lyscprice_com.png',
+                    //   backgroundSize: 'cover',
+                    //   backgroundPosition: 'center -7vh',
+                    // },
                     // {
                     //   type: 'image.image',
                     //   title: '中国·金乡大蒜指数',
@@ -152,86 +152,84 @@ var config = {
                     //   backgroundSize: 'cover',
                     //   backgroundPosition: 'center -7vh',
                     // },
+                    // {
+                    //   type: 'image.image',
+                    //   title: '中国·寿光蔬菜指数',
+                    //   url: 'https://github.com/7253641/mozaik-demo/raw/demo/www_sgvindex_com.png',
+                    //   backgroundSize: 'cover',
+                    //   backgroundPosition: 'center -7vh',
+                    // },
+                    // {
+                    //   type: 'image.image',
+                    //   title: '中国·成都中药材指数',
+                    //   url: 'https://github.com/7253641/mozaik-demo/raw/demo/www_ysindex_com.png',
+                    //   backgroundSize: 'cover',
+                    //   backgroundPosition: 'center -7vh',
+                    // },
                     {
-                      type: 'image.image',
-                      title: '中国·寿光蔬菜指数',
-                      url: 'https://github.com/7253641/mozaik-demo/raw/demo/www_sgvindex_com.png',
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center -7vh',
+                      type: 'iframe.iframe',
+                      title: '中国·义乌小商品指数',
+                      url: 'http://www.ywindex.com'
                     },
                     {
-                      type: 'image.image',
-                      title: '中国·成都中药材指数',
-                      url: 'https://github.com/7253641/mozaik-demo/raw/demo/www_ysindex_com.png',
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center -7vh',
+                      type: 'iframe.iframe',
+                      title: '中国·柯桥纺织指数',
+                      url: 'http://www.kqindex.cn'
                     },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·义乌小商品指数',
-                    //   url: 'http://www.ywindex.com'
-                    // },
-
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·柯桥纺织指数',
-                    //   url: 'http://www.kqindex.cn'
-                    // },
-
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·海宁皮革指数',
-                    //   url: 'http://www.hnindex.com'
-                    // },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·舟山水产品指数',
-                    //   url: 'http://www.scpzs.com/'
-                    // },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·永康五金指数',
-                    //   url: 'http://www.ykindex.com'
-                    // },
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·海宁皮革指数',
+                      url: 'http://www.hnindex.com'
+                    },
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·舟山水产品指数',
+                      url: 'http://www.scpzs.com/'
+                    },
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·永康五金指数',
+                      url: 'http://www.ykindex.com'
+                    },
                     // {
                     //   type: 'iframe.iframe',
                     //   title: '中国·四季青服装指数',
                     // },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·金恒德汽车用品价格指数',
-                    //   url: 'http://www.jhdindex.com'
-                    // },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·中关村电子信息产品指数',
-                    //   url: 'http://www.zgcindex.org'
-                    // },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·临沂商城价格指数',
-                    //   url: 'http://www.lyscprice.com'
-                    // },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·金乡大蒜指数',
-                    //   url: 'http://www.jxdsi.com'
-                    // },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·寿光蔬菜指数',
-                    //   url: 'http://www.sgvindex.com'
-                    // },
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·金恒德汽车用品价格指数',
+                      url: 'http://www.jhdindex.com'
+                    },
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·中关村电子信息产品指数',
+                      url: 'http://www.zgcindex.org'
+                    },
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·临沂商城价格指数',
+                      url: 'http://www.lyscprice.com'
+                    },
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·金乡大蒜指数',
+                      url: 'http://www.jxdsi.com'
+                    },
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·寿光蔬菜指数',
+                      url: 'http://www.sgvindex.com'
+                    },
                     // {
                     //   type: 'iframe.iframe',
                     //   title: '中国·昌邑生姜指数',
                     //   url: '',
                     // },
-                    // {
-                    //   type: 'iframe.iframe',
-                    //   title: '中国·成都中药材指数',
-                    //   url: 'http://www.ysindex.com',
-                    // },
+                    {
+                      type: 'iframe.iframe',
+                      title: '中国·成都中药材指数',
+                      url: 'http://www.ysindex.com',
+                    },
                   ]
               },
               {
