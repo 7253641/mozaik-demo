57d77c4ae301f9a81fb2b5122917557192a6da9a
zhangtao <7253641@qq.com>
2019-01-09 15:11:02 +0800

    size

--- src/chart/ui/components/BarChartWidget.jsx
+++ src/chart/ui/components/BarChartWidget.jsx
@@ -19,12 +19,12 @@ class BarChartWidget extends Component {
             yLegend: '数 量',
             yLegendPosition: 'top',
             xPadding: 5,
-            margin:             {
+            margin: {
               top:    20,
               right:  20,
               bottom: 20,
               left:   20
-          }
+            },
             barColor: "#aadcff",
             barClass: function (d) {
                 return `result--${ builds }`;
